45246bb49e29332ec72e719e1fb84e3ebd9eb2ea
MG <gmelina69@gmail.com>
2026-05-01 13:34:50 +0400

    Уронить тест проверки запроса POST

---
+++ src/test/java/ru/netology/hw/api/PostmanEchoTest.java
@@ -0,0 +1,25 @@
+package ru.netology.hw.api;
+
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
+
+public class PostmanEchoTest {
+
+    @Test
+    void shouldToSendRequest() {
+        given()
+                .baseUri("https://postman-echo.com")
+                .body("some data")
+                .when()
+                .log()
+                .all()
+                .post("/post")
+                .then()
+                .log()
+                .all()
+                .statusCode(200)
+                .body("data", equalTo("some"));
+    }
+}
